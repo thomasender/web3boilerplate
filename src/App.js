653b2cce72a2caca9c6d66dbb79e51d6147a9f86
Thomas Ender <freisingen@yahoo.com>
2021-06-11 14:21:35 +0200

    useState useEffect Setup, Fetching tokenData

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import './App.css';
 import Web3 from 'web3';
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { abi } from './abis/abi';
 
-function App() {
-  var web3;
-  var accounts;
-  var user;
-  var balance;
-  
+const App = () => {
+
+  const [userAddress, setUserAddress] = useState("");
+  const [tokenName, setTokenName] = useState("");
+
+  useEffect(() => {
+    initWeb3();
+  }, []);
+
   const initWeb3 = async () =>{
 
-  web3 = null;
-  
+  let web3 = null;
+  let accounts;
+  const contractAddress = "0x722dd3F80BAC40c951b51BdD28Dd19d435762180";
+  let token;
+
     if(!window.ethereum){
       alert("Please install MetaMask");
+      window.open("https://metamask.io/");
     }
 
     if(typeof web3 !== 'undefined'){
       web3 = new Web3(window.ethereum);
-      console.log(web3);
+      await window.ethereum.request({
+        method: "wallet_requestPermissions",
+        params: [
+          {
+            eth_accounts: {}
+          }
+        ]
+      });
       accounts = await web3.eth.getAccounts();
-      user = accounts[0];
-      console.log("usER", user);
-      balance = await web3.eth.getBalance(user);
-      console.log(balance);
-    } else {
-      // set the provider you want from Web3.providers
-      alert("Please install Metamask");
-      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
-
+      if(accounts && accounts.length){
+        setUserAddress(accounts[0]);
+      }
+      token = new web3.eth.Contract(abi, contractAddress);
+        setTokenName( async () => {
+          if(token){
+            await token.methods.name().call()
+          }
+        })
+  } else {
+    alert("")
   }
-  
-  initWeb3();
+}
+
   return (
     <div className="App">
-      { web3 ? "Its there" : "Not there" }
-      { user ? "Yep" : "nope"}
+        
     </div>
   );
-}
+
+} 
 
 export default App;
